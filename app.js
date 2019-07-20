cc4c3efe2cfbdf77c6b4d46e42db7475fdc7bdb8
RCLU30 <15187974+RCLU30@users.noreply.github.com>
2019-07-20 12:01:50 -0400

    first commit

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-/* sample files here - feel free to use your own setup/generator */
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(process.env.PORT, process.env.IP, function() {
+	console.log('server is running...');
+});
---
+++ keys.js
@@ -0,0 +1,5 @@
+// keys.js - figure out which credentials to return
+// create a file 'dev.js' that holds the API key
+
+module.exports = require('./dev');
+
