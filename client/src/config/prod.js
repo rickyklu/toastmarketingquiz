8d14d865029ba577b07ba0acd65f1690f0810b52
RCLU30 <15187974+RCLU30@users.noreply.github.com>
2019-07-22 02:37:51 -0400

    fix env variables

--- client/src/config/prod.js
+++ client/src/config/prod.js
@@ -1,4 +1,4 @@
 module.exports = {
 	darkSkyKey: process.env.DARK_SKY_KEY,
-	googlePlacesKey: process.env.GOOGLE_PLACES_KEY
+	googlePlacesKey: process.env.REACT_APP_GOOGLE_PLACES_KEY
 };
--- config/prod.js
+++ config/prod.js
@@ -1,3 +1,4 @@
 module.exports = {
+	darkSkyKey: process.env.DARK_SKY_KEY,
 	googlePlacesKey: process.env.REACT_APP_GOOGLE_PLACES_KEY
 };
